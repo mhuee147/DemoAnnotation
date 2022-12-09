3d131ad5f2a183ec137340484db809a5b211de6c
MinhHue <minhhue14072002@gmail.com>
2022-12-09 16:16:52 +0700

    add files

--- Annotation-main/src/main/java/com/example/annotation/Lesson1/Student.java
+++ Annotation-main/src/main/java/com/example/annotation/Lesson1/Student.java
@@ -1,6 +1,5 @@
 package com.example.annotation.Lesson1;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
